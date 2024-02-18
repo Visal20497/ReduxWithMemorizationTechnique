55bc6161bcdec7e09c59ad7e5bb014966c2acb95
Visal20497 <visal20497@gmail.com>
2024-02-19 02:03:05 +0530

    Memorization

---
+++ src/App.js
@@ -0,0 +1,25 @@
+ import React from 'react'
+import Counter from './Counter'
+import Counter1 from './Counter1'
+import Print2 from './Print2'
+import Card from './Card'
+import Fetch from './Fetch'
+ 
+ function App() {
+   return (
+     <div>App
+      {/* <Counter/> */}
+      <Counter1/>
+      <hr/>
+      <Print2/>
+      <hr/>
+      <br/>
+      <Card/>
+      <hr/>
+      <br/>
+      <Fetch/>
+     </div>
+   )
+ }
+ 
+ export default App
